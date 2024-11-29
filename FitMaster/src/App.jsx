3717023df741ab5e24f77cb3f1c6afdda8658eb5
Christopher Castillo <christopher.a.castillo.b@gmail.com>
2024-11-29 17:54:16 -0500

    Pull request resolve: Routing

--- FitMaster/src/App.jsx
+++ FitMaster/src/App.jsx
@@ -1,75 +1,25 @@
-import { useState, useEffect } from 'react';
-import UserList from "./UserList.jsx";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import './App.css';
-import UserForm from "./UserForm.jsx";
-import Page1 from "./Page1.jsx";
-import Page2 from "./Page2.jsx";
 import BMICalculator from "./BMICalculator";
 import Authentication from './Authentication.jsx';
 import RestaurantOptions from './RestaurantOptions.jsx';
+import Page1 from "./Page1.jsx";
+
 
 function App() {
-    const [users, setUsers] = useState([]);
-    const [isModalOpen, setIsModalOpen] = useState(false);
-    const [currentUser, setCurrentUser] = useState({});
-
-    useEffect(() => {
-       fetchUsers()
-    }, []);
-
-    const fetchUsers = async () => {
-        const response = await fetch("http://127.0.0.1:5000/api/users");
-        const data = await response.json();
-        setUsers(data.users);
-        console.log(data.users);
-    }
-
-    const closeModal = () => {
-        setIsModalOpen(false);
-        setCurrentUser({});
-    }
-
-    const onUpdate = () => {
-        closeModal();
-        fetchUsers();
-    }
-
-    const openCreateModal = () => {
-        if (!isModalOpen) setIsModalOpen(true);
-    }
-
-    const openEditModal = (user) => {
-        if (isModalOpen) return
-        setCurrentUser(user)
-        setIsModalOpen(true);
-
-    }
   return (<>
-      <UserList users={users} updateUser={openEditModal} updateCallback={onUpdate} />
-          <button onClick={openCreateModal}>Create New Contact</button>
-          {isModalOpen && <div className="modal">
-              <div className="modal-content">
-                  <span className="close" onClick={closeModal}>&times;</span>
-                  <UserForm existingUser={currentUser} updateCallback={onUpdate} />
-              </div>
+          <div>
+              <BrowserRouter>
+                  <Routes>
+                      <Route index element={<Home/>}/>
+                      <Route path="/home" element={<Home/>}/>
+                      <Route path="/BMICalculator" element={<BMICalculator/>}/>
+                      <Route path="/RestaurantOptions" element={<RestaurantOptions/>}/>
+                      <Route path="/Authentication" element={<Authentication/>}/>
+                  </Routes>
+              </BrowserRouter>
           </div>
-
-          }
-          {/* BMI Calculator Section */}
-          <div style={{ marginTop: "20px" }}>
-                <BMICalculator />
-          </div>
-
-          {/* Restaurant Options Section */}
-          <div style={{ marginTop: "20px" }}>
-                <RestaurantOptions />
-          </div>
-
-          {/* Authentication Section */}
-          <div style={{ marginTop: "20px" }}>
-                <Authentication />
-          </div>
-  </>
+      </>
   );
 }
 
